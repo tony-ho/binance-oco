b47ad75ee21fac60cd7dd21ee822e360d6cfb084
Tony Ho <tony-ho@users.noreply.github.com>
2019-05-18 16:41:08 +1000

    test: add ts-jest

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  globals: {
+    "ts-jest": {
+      tsConfig: "tsconfig.json"
+    }
+  },
+  moduleFileExtensions: ["ts", "js"],
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest"
+  },
+  testEnvironment: "node"
+};
